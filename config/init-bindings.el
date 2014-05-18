d445dcc7dec628634deba2c78b47967b3c5f85dc
Samim Pezeshki <p.samim@gmail.com>
2014-05-18 14:08:08 +0430

    Nearly all the Evil keys in Persian keyboard layout

--- config/init-bindings.el
+++ config/init-bindings.el
@@ -119,24 +119,6 @@
   (define-key evil-motion-state-map "j" 'evil-next-visual-line)
   (define-key evil-motion-state-map "k" 'evil-previous-visual-line)
 
-;; Samim's confs
-  (define-key evil-motion-state-map "ت" 'evil-next-visual-line)
-  (define-key evil-motion-state-map "ن" 'evil-previous-visual-line)
-  (define-key evil-motion-state-map "ؤ" 'evil-append-line)
-  (define-key evil-motion-state-map "ا" 'evil-forward-char)
-  (define-key evil-motion-state-map "م" 'evil-backward-char)
-  (define-key evil-motion-state-map "ه" 'evil-insert)
-  (define-key evil-motion-state-map "ه" 'evil-insert)
-  (define-key evil-motion-state-map "ی" 'evil-delete)
-  (define-key evil-motion-state-map "ط" 'evil-delete-char)
-  (define-key evil-motion-state-map "ع" 'undo-tree-undo)
-  (define-key evil-motion-state-map "خ" (bind (evil-insert-newline-below) (forward-line -1)))
-  (global-set-key (kbd "C-ص") 'evil-window-map)
-  (define-key evil-normal-state-map (kbd "C-ا") 'evil-window-left)
-  (define-key evil-normal-state-map (kbd "C-ت") 'evil-window-down)
-  (define-key evil-normal-state-map (kbd "C-ن") 'evil-window-up)
-  (define-key evil-normal-state-map (kbd "C-م") 'evil-window-right)
-
   (define-key evil-normal-state-map (kbd "Q") 'my-window-killer)
   (define-key evil-normal-state-map (kbd "Y") (kbd "y$"))
 
@@ -285,5 +267,32 @@
 
 ;; Samim's confs
 (global-set-key (kbd "<f11>") 'switch-full-screen)
+
+;; Farsi keyboard layout bindings
+(after 'evil
+  (define-key evil-motion-state-map "ت" 'evil-next-visual-line)
+  (define-key evil-motion-state-map "ن" 'evil-previous-visual-line)
+  (define-key evil-motion-state-map "ؤ" 'evil-append-line)
+  (define-key evil-motion-state-map "ا" 'evil-forward-char)
+  (define-key evil-motion-state-map "م" 'evil-backward-char)
+  (define-key evil-motion-state-map "ه" 'evil-insert)
+  (define-key evil-motion-state-map "ه" 'evil-insert)
+  (define-key evil-motion-state-map "ی" 'evil-delete)
+  (define-key evil-motion-state-map "ط" 'evil-delete-char)
+  (define-key evil-motion-state-map "ع" 'undo-tree-undo)
+  (define-key evil-motion-state-map "خ" (bind (evil-insert-newline-below) (forward-line -1)))
+  (define-key evil-normal-state-map "غ" 'evil-yank)
+  (define-key evil-normal-state-map "ح" 'evil-paste-after)
+  (define-key evil-normal-state-map "ر" 'evil-visual-char)
+  (define-key evil-normal-state-map "ٰ" 'evil-visual-line)
+  (define-key evil-normal-state-map "ش" 'evil-append)
+  (define-key evil-normal-state-map "ؤ" 'evil-append-line)
+  (define-key evil-normal-state-map "ز" 'evil-change)
+  (define-key evil-normal-state-map "ژ" 'evil-change-line)
+  (define-key evil-normal-state-map "ي" 'evil-delete-line)
+  (define-key evil-normal-state-map (kbd "C-ا") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-ت") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-ن") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-م") 'evil-window-right))
 
 (provide 'init-bindings)
