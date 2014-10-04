00583666f96fdd9f9dbddd3c29e5b32068a9980c
Bersam Karbasion <bersam.k@gmail.com>
2014-10-04 19:23:12 +0330

    Revert "add google cpplint to flycheck"
    This reverts commit ea2f0b86167b9af13e21ff70291e13be6404c6a1.

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -8,23 +8,4 @@
 
 (global-flycheck-mode t)
 
-;; Bersam's Configs
-(require-package 'flycheck-google-cpplint)
-(eval-after-load 'flycheck
-  '(progn
-     (require 'flycheck-google-cpplint)
-     ;; Add Google C++ Style checker.
-     ;; In default, syntax checked by Clang and Cppcheck.
-     (flycheck-add-next-checker 'c/c++-cppcheck
-                                '(warnings-only . c/c++-googlelint))))
-
-(custom-set-variables
- '(flycheck-c/c++-googlelint-executable "/usr/bin/cpplint"))
-
-(custom-set-variables
- '(flycheck-googlelint-verbose "3")
- '(flycheck-googlelint-filter "-whitespace,+whitespace/braces")
- '(flycheck-googlelint-root "project/src")
- '(flycheck-googlelint-linelength "120"))
-
 (provide 'init-flycheck)
