89ae08fd6d929045536455ee319f1c8c686bacad
Bersam Karbasion <bersam.k@gmail.com>
2014-10-06 14:29:06 +0330

    add config to bury compile buffer if successful

--- config/init-misc.el
+++ config/init-misc.el
@@ -83,5 +83,20 @@
 ;; Bersam's confs
 (add-hook 'prog-mode-hook (lambda ()
                             (local-set-key (kbd "M-RET") 'evil-goto-definition)))
+;; bury buffer if successful compile 
+(defun bury-compile-buffer-if-successful (buffer string)
+  "Bury a compilation buffer if succeeded without warnings "
+  (if (and
+       (string-match "compilation" (buffer-name buffer))
+       (string-match "finished" string)
+       (not
+        (with-current-buffer buffer
+          (search-forward "warning" nil t))))
+      (run-with-timer 1 nil
+                      (lambda (buf)
+                        (bury-buffer buf)
+                        (switch-to-prev-buffer (get-buffer-window buf) 'kill))
+                      buffer)))
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
 
 (provide 'init-misc)
