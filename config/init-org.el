31c4c59e0048582ee939fbb3e4c4281103b937eb
Samim Pezeshki <p.samim@gmail.com>
2015-05-05 15:03:38 +0430

    Add another latex class

--- config/init-org.el
+++ config/init-org.el
@@ -288,6 +288,39 @@
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                ("\\paragraph{%s}" . "\\paragraph*{%s}")))
 
+(add-to-list 'org-latex-classes
+             '("assignment-unno"
+               "\\documentclass[11pt,a4paper]{article}
+\\usepackage[utf8]{inputenc}
+\\usepackage[T1]{fontenc}
+\\usepackage{fixltx2e}
+\\usepackage{graphicx}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{wrapfig}
+\\usepackage{rotating}
+\\usepackage[normalem]{ulem}
+\\usepackage{amsmath}
+\\usepackage{textcomp}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{amssymb}
+\\usepackage{hyperref}
+\\usepackage{mathpazo}
+\\usepackage{color}
+\\usepackage{enumerate}
+\\definecolor{bg}{rgb}{0.95,0.95,0.95}
+\\tolerance=1000
+      [NO-DEFAULT-PACKAGES]
+      [PACKAGES]
+      [EXTRA]
+\\linespread{1.1}
+\\hypersetup{pdfborder=0 0 0}"
+               ("\\section*{%s}" . "\\section{%s}")
+               ("\\subsection*{%s}" . "\\subsection{%s}")
+               ("\\subsubsection*{%s}" . "\\subsubsection{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")))
+
 
 ;; RefTeX formats for biblatex (not natbib)
 ;; C-c [ and one the below citing styles
