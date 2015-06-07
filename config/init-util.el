6cd07862668d26c944619b4a05783587be69d5c3
Samim Pezeshki <p.samim@gmail.com>
2015-06-07 11:44:19 +0430

    Turn off mouse when on Emacs

--- config/init-util.el
+++ config/init-util.el
@@ -203,4 +203,16 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (find-file "~/Notes/todo.org")
   (writeroom-mode))
 
-  (provide 'init-util)
+(defun turn-off-mouse (&optional frame)
+  (interactive)
+  (shell-command "synclient TouchpadOff=1"))
+
+(defun turn-on-mouse (&optional frame)
+  (interactive)
+  (shell-command "synclient TouchpadOff=0"))
+
+(add-hook 'focus-in-hook #'turn-off-mouse)
+(add-hook 'focus-out-hook #'turn-on-mouse)
+(add-hook 'delete-frame-functions #'turn-on-mouse)
+
+(provide 'init-util)
